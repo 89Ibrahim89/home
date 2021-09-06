a53c9a3675fe99080f36758543110ef95ce8a6ca
ibrahim <itolobaev@gmail.com>
2021-09-06 15:06:15 +0600

    первая домашка

---
+++ src/com/company/Main.java
@@ -0,0 +1,75 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        String word = "Salam bro ";
+//
+//        final int num = 89;
+//
+//        word = word + num;
+//
+//        System.out.println(word);
+//
+//        if (num < 0) {
+//            System.out.println("Вы сохранили отрицательное число");
+//        } else if (num > 0) {
+//            System.out.println("Вы сохранили положительное число");
+//        }
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Введите ваше имя");
+//        System.out.println("всем привет");
+//        System.out.println("hi");
+//        .out.println("a+b="+(2+3));
+//        System.out.println("(a + b)*2 = "+(2+3)*2);
+//        System.out.println(89 + 99 + 100);
+//        System.out.println((89 + 99)*(100+89));
+//        System.out.println("(a+b)"+(2+3));
+//        int a = 89;
+//        int b = 99;
+//        System.out.println("a + b ="+(a+b));
+//        System.out.println(a+("+ b =")+(a+b));
+//        System.out.println(a+(" +"+ b+" =")+(a+b));
+//        System.out.println(a+" +"+ b+" ="+(a+b));
+
+
+//          Scanner scn = new Scanner(System.in);
+//          int val = scn.nextInt();
+//        System.out.println("val ="+val);
+//        System.out.println("Enter a");
+//        int a = scn.nextInt();
+//        System.out.println("Enter b");
+//        int b = scn.nextInt();
+//        System.out.println(a+" + "+b+" = "+(a+b));
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter input");
+//        int a = input.nextInt();
+//        System.out.println("a = "+a);
+
+        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Enter sum");
+//        double x = 5;
+//        double y = 8;
+//        System.out.println(x/y);
+//        double sum = input.nextDouble();
+//        System.out.println("Enter curs");
+//        double curs = input.nextDouble();
+//        System.out.println("sum in rub = "+sum*curs);
+//
+        System.out.println("Введите ваше имя");
+        String имя = input.next();
+        System.out.println("Введите вашу фамилию");
+        String фамилия = input.next();
+        System.out.println("имя :"+имя);
+        System.out.println("фамилия:"+ фамилия);
+
+    }
+}
+
+
